326c45ec3558357711ca2d15a4f0be02b5950484
SowreeshUndru <undrusowreesh@gmail.com>
2025-10-29 01:15:16 +0530

    Clean up testhook.js by removing comments
    Removed unnecessary comments and extraneous text.

--- testhook.js
+++ testhook.js
@@ -8,9 +8,3 @@ console.log("✅ GitHub push test successful at:", new Date().toLocaleString());
 // my name ?fgsffgdfffffffffdgggggfffgdf?ddffffffffffffhellodgfgfffffffsjdghfgsgdfdfdddffffffffffffdff
 
 
-//my name is sowreesh Undruffff
-//lokesh
-
-uuuuuuuuuuu
-//hello my name is chimpu how are uuu uuuuuuuuuuuuuuu
-uuuujuuuuuuuuuuuuuuuuu
